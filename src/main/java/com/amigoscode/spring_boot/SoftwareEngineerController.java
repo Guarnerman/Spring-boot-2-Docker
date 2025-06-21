e0d48f68edc053cfba7d75202dd0ddaf3294eff5
Ulises GS <gs.ulises18@gmail.com>
2025-06-20 19:07:17 -0600

    Adición de JPA para usar la interfaz Repository y separamos el funcionamiento entre Service y Conroller respectivamente. Adición de un endpoint para hacer POST de nuuevo softwareEngineer

--- src/main/java/com/amigoscode/spring_boot/SoftwareEngineerController.java
+++ src/main/java/com/amigoscode/spring_boot/SoftwareEngineerController.java
@@ -1,8 +1,6 @@
 package com.amigoscode.spring_boot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -10,12 +8,18 @@ import java.util.List;
 @RequestMapping("/api/v1/software-engineers")
 public class SoftwareEngineerController {
 
+    private final SoftwareEngineerService softwareEngineerService;
+
+    public SoftwareEngineerController(SoftwareEngineerService softwareEngineerService){
+        this.softwareEngineerService = softwareEngineerService;
+    }
+
     @GetMapping
-    public List<SoftwareEngineer> getEngineers() {
-        return List.of(
-                new SoftwareEngineer(1, "Mario", "JS, Node, React, TailwindCSS"),
-                new SoftwareEngineer(2, "Yisus", "JS, Node, Angular, MaterialUI"),
-                new SoftwareEngineer(3, "Paul", "Java, Spring, Sprong-Boot")
-        );
+    public List<SoftwareEngineer> getAllEngineers() {
+        return softwareEngineerService.getAllSoftwareEngineers();
+    }
+    @PostMapping
+    public void addNewSoftwareEngineer(@RequestBody SoftwareEngineer softwareEngineer){
+        softwareEngineerService.insertSoftwareEngineer(softwareEngineer);
     }
 }
